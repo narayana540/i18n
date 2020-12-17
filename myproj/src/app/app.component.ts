3a1bd73fdf16f242268d201eb9d14bd4f17a5220
Narayana Jadapally <narayana.j08@gmail.com>
2020-12-17 11:24:50 +0530

    first commit

---
+++ myproj/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(public translate:TranslateService) {
+    translate.addLangs(['en','fr','zh','hi-IN']);
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|fr/) ? browserLang :'en')
+
+  }
+  
+}
